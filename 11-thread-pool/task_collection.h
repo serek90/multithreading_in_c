c748bcd72d00eab8b04804e2377c75ee48e3d56b
Jakub Seroczynski <jak.seroczynski@gmail.com>
2026-05-20 22:18:57 +0200

    11-thread-pool: Implement simple idea

---
+++ 11-thread-pool/task_collection.h
@@ -0,0 +1,13 @@
+#ifndef _TASK_COLLECTION_H
+#define _TASK_COLLECTION_H
+
+void number_mult(int num) {
+    printf("number_mult: %d\n", num * num);
+}
+
+void number_sum(int num) {
+    printf("number_sum: %d\n", num + num);
+}
+
+
+#endif
--- 11-thread-pool/thread_pool.c
+++ 11-thread-pool/thread_pool.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
+#include "task_collection.h"
 
 typedef struct s_task {
     void (*task)(int);
@@ -7,22 +9,55 @@ typedef struct s_task {
 } task_t;
 
 #define POOL_SIZE 4
-pthread_t thread_pool[POOL_SIZE];
+#define QUEUE_SIZE 20
+
+typedef struct thread_pool_s {
+    pthread_t workers[POOL_SIZE];
+    task_t task_queue[QUEUE_SIZE];
+    int queue_idx;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+} thread_pool_t;
+
+thread_pool_t thread_pool;
 
 void *thread_func(void *args) {
+    printf("Worker %d start performing tasks\n", *(int *)args);
+    while(1) {
+        pthread_mutex_lock(&thread_pool.mutex);
+        int idx = thread_pool.queue_idx;
+        // if queue empty, write API to queue
+        thread_pool.queue_idx++;
+        pthread_mutex_unlock(&thread_pool.mutex);
+        sleep(10);
+        thread_pool.task_queue[idx].task(idx);
+    }
     return NULL;
 }
 
-void thread_run(void) {
+void thread_pool_queue_fill(thread_pool_t *pool) {
+    for(int i = 0; i < QUEUE_SIZE; i++) {
+        pool->task_queue[i].task = i%2 ? number_sum : number_mult;
+        pool->task_queue[i].arg = i;
+    }
+}
+
+void thread_pool_run(void) {
+
+    int thread_args[POOL_SIZE];
+
     for(int i = 0; i < POOL_SIZE; i++) {
-        pthread_create(&thread_pool[i], NULL, thread_func, NULL);
+        thread_args[i] = i;
+        pthread_create(&thread_pool.workers[i], NULL, thread_func, &thread_args[i]);
     }
 
     for(int i = 0; i < POOL_SIZE; i++)
-        pthread_join(thread_pool[i], NULL);
+        pthread_join(thread_pool.workers[i], NULL);
 }
 
 
 int main() {
+    thread_pool_queue_fill(&thread_pool);
+    thread_pool_run();
     return 0;
 }
